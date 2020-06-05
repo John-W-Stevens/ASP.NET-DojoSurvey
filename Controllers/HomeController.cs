e1a1b7dff9096f01f6ebd86e68a2c158007913a3
John Stevens <john.william.stevens1@gmail.com>
2020-06-04 18:32:22 -0700

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DojoSurvey.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: ""/
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // POST: ""/
+        [HttpPost("")]
+        public IActionResult Result(string Name, string Location, string Language, string Comment)
+        {
+
+            ViewBag.Name = Name;
+            ViewBag.Location = Location;
+            ViewBag.Language = Language;
+            ViewBag.Comment = Comment;
+            return View();
+
+        }
+
+    }
+}
